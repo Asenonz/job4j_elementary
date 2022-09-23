a925fbbda42a75b5619c175a1961d287a42c90d1
Asenonz <Asenonz@mail.ru>
2022-09-23 11:11:37 +0700

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double sumDifAndDiv(double first, double second) {
+        return dif(first, second) + div(first, second);
+    }
+
+    public static double sumAllMethods(double first, double second) {
+        return sum(first, second) + multiply(first, second)
+                + dif(first, second) + div(first, second);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Сумма всех методов: " + sumAllMethods(6, 3));
+
+    }
+}
+
