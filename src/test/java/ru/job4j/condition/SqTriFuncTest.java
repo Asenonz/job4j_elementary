ead2b103ca8d0895374ae9819a301716ec48d84b
Asenonz <Asenonz@mail.ru>
2022-09-27 17:12:12 +0700

    1.2. Входные данные в тесте [#297750 #345917]

--- src/test/java/ru/job4j/condition/SqTriFuncTest.java
+++ src/test/java/ru/job4j/condition/SqTriFuncTest.java
@@ -6,7 +6,10 @@ import org.junit.Assert;
 public class SqTriFuncTest {
     @Test
     public void whenA1B1C1X1Then3() {
-        int a = 1, b = 1, c = 1, x = 1;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
         int expected = 3;
         int rsl = SqTriFunc.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
@@ -14,7 +17,10 @@ public class SqTriFuncTest {
 
     @Test
     public void whenA0B1C1X1Then2() {
-        int a = 0, b = 1, c = 1, x = 1;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
         int expected = 2;
         int rsl = SqTriFunc.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
@@ -22,7 +28,10 @@ public class SqTriFuncTest {
 
     @Test
     public void whenA1B1C0X1Then2() {
-        int a = 1, b = 1, c = 0, x = 1;
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
         int expected = 2;
         int rsl = SqTriFunc.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
@@ -30,8 +39,11 @@ public class SqTriFuncTest {
 
     @Test
     public void whenA1B1C01X0Then1() {
-        int a = 1, b = 1, c = 0, x = 1;
-        int expected = 2;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
         int rsl = SqTriFunc.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
