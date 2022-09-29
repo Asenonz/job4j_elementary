93fcebd5f9258d241f0f711dd069619e753e075c
Asenonz <Asenonz@mail.ru>
2022-09-30 02:24:12 +0700

    4.4. Вложенный оператор if else. Запутанный код (Переписал на английском языке)[#190]

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -4,13 +4,13 @@ public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
         if ((number % 3) == 0  && (number % 2) == 0) {
-            rsl = "Исходное число делится на 6.";
+            rsl = "Number is divisible by 6.";
         } else if ((number % 3) == 0 && ((number % 2) != 0)) {
-            rsl = "Исходное число делится на 3, но не является четным.";
+            rsl = "The number is divisible by 6 but is odd.";
         } else if (((number % 3) != 0) && ((number % 2) == 0)) {
-            rsl = "Исходное число не делится на 3, но является четным.";
+            rsl = "Number is not divisible by 3 but is even.";
         } else {
-            rsl = "Исходное число не делится на 3 и не является четным.";
+            rsl = "Number is not divisible by 3 and is not even.";
         }
         return rsl;
     }
