79f828f473b18f6b2402f105d6894d8d00d6afe7
Asenonz <Asenonz@mail.ru>
2022-10-12 12:41:17 +0700

    6.4.3. Слово заканчивается на ... [#156317 #350954]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -5,6 +5,7 @@ public class EndsWith {
         boolean result = true;
         for (int i = 0; i < post.length; i++) {
             if (word[word.length - i - 1] != post[post.length - i - 1]) {
+                result = false;
                 break;
             }
         }
