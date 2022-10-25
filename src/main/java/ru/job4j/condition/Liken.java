70232497bfa84c1df377babc406ae7aa308c6027
Asenonz <Asenonz@mail.ru>
2022-10-25 15:21:20 +0700

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -4,14 +4,21 @@ public class Liken {
     public static void main(String[] args) {
         int first = 10;
         int second = 9;
-
-        boolean result = first > second;
-                System.out.println(result);
-
-        result = first < second;
-                System.out.println(result);
-
-        result = first == second;
-                System.out.println(result);
+        System.out.println(firstMoreSecond(first, second));
+        System.out.println(firstLessSecond(first, second));
+        System.out.println(firstEqualsSecond(first, second));
     }
+
+    public static boolean firstMoreSecond(int first, int second) {
+        return first > second;
+    }
+
+    public static boolean firstLessSecond(int first, int second) {
+        return first < second;
+    }
+
+    public static boolean firstEqualsSecond(int first, int second) {
+        return first == second;
+    }
+
 }
